687f9f81eb965629e10e97724f1750d9f4da0da0
BEN OKELLO MWAKA <mwakaben@gmail.com>
2023-10-18 16:23:37 +0300

    adding a bae line and horizontal grid to 3 buttons plus actions

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.colormyviews.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -23,7 +22,9 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(binding.boxOneText,binding.boxTwoText,binding.boxThreeText,
-                binding.boxFourText,binding.boxFiveText, binding.constraintLayout)
+                binding.boxFourText,binding.boxFiveText, binding.constraintLayout,
+            binding.yellowButton, binding.greenButton,binding.redButton
+            )
 
             for (item in clickableViews){
                 item.setOnClickListener{makeColored(it)}
@@ -42,6 +43,14 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
+
+
+
+            R.id.red_button -> binding.boxFiveText.setBackgroundResource(R.color.my_red)
+            R.id.green_button -> binding.boxFourText.setBackgroundResource(R.color.my_green)
+            R.id.yellow_button -> binding.boxThreeText.setBackgroundResource(R.color.my_yellow)
+
+
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
