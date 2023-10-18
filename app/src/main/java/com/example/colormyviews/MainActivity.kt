6c25deff3cb1171a8777dbb3ab756f9565bed2ae
BEN OKELLO MWAKA <mwakaben@gmail.com>
2023-10-18 15:02:13 +0300

    begin Baselines

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,20 +4,29 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.colormyviews.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
         setListeners()
-    }
 
+    }
+//
     private fun setListeners() {
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text,box_three_text,box_four_text,box_five_text)
+            listOf(binding.boxOneText,binding.boxTwoText,binding.boxThreeText,
+                binding.boxFourText,binding.boxFiveText, binding.constraintLayout)
 
-            for (Item in clickableViews){
-                Item.setOnClickListener(makeColored(it))
+            for (item in clickableViews){
+                item.setOnClickListener{makeColored(it)}
             }
     }
 
@@ -32,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             // Boxes using Android color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
---
+++ app/src/main/java/com/example/colormyviews/box.kt
@@ -0,0 +1,3 @@
+package com.example.colormyviews
+
+data class box(val box_id :String, val name : String, val age: Int)
